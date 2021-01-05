c72d14ab3d2e7f3a2ca74f1c7b6a94bdf41e7acb
LimonkaTM <etuzin@mail.ru>
2021-01-05 19:49:33 +0300

    ff

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-let	neverLucky='Never Lucky';
+let	neverLucky = 'Never Lucky';
+	neverGood = 10 + 2;
 
 alert(neverLucky);
 document.write(neverLucky);
 console.log(neverLucky);
+
+alert(neverGood);
+document.write(neverGood);
+console.log(neverGood);
