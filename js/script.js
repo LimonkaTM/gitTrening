85c70aff94d209eb3f547e6fb5213486e5198658
LimonkaTM <etuzin@mail.ru>
2021-06-06 23:48:17 +0300

    js file

--- js/script.js
+++ js/script.js
@@ -1,10 +1,1 @@
-let	neverLucky = 'Never Lucky';
-	neverGood = 10 + 2;
-
-alert(neverLucky);
-document.write(neverLucky);
-console.log(neverLucky);
-
-alert(neverGood);
-document.write(neverGood);
-console.log(neverGood);
+'use strict';
